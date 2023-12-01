dabb789708654ee0f44dcd79febc6fa5e03c4a70
namankhurpia <naman.khurpia2016@vitalum.ac.in>
2023-12-01 02:02:56 -0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+/*!
+* Start Bootstrap - Landing Page v6.0.6 (https://startbootstrap.com/theme/landing-page)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+//script
+//https://script.google.com/macros/s/AKfycbxFxNhV_eK41SLeXIEVHplf-essp9hiWh8ue9bOWoD6jHVNQtD4eAc4x0Sz0lnol5x_/exec
